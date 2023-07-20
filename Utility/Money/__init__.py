90e3f1a6dbd9d1b0e01d26f4d57d1cc875e139a1
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-19 22:43:33 -0300

    Initial commit.

---
+++ Utility/Money/__init__.py
@@ -0,0 +1,32 @@
+def increase(price=0, tax=0, format=False):
+    result = price + (price * tax / 100)
+    return result if format is False else money(result)
+
+
+def decrease(price=0, tax=0, format=False):
+    result = price - (price * tax / 100)
+    return result if format is False else money(result)
+
+
+def double(price=0, format=False):
+    result = price * 2
+    return result if not format else money(result)
+
+def half(price=0, format=False):
+    result = price / 2
+    return result if not format else money(result)
+
+def money(price=0, money='$'):
+    return f'{money}{price:>.2f}'.replace(',', '.')
+
+def resume(price=0, tax=10, taxing=5):
+    print('-' * 30)
+    print('RETURN OF THE VALUE'.center(30))
+    print('-' * 30)
+    print(f'Price analyzed: \t\t\033[1;33m{money(price)}\033[m')
+    print(f'Double of the price: \t\033[1;32m{double(price, True)}\033[m')
+    print(f'Half of the price: \t\t\033[1;31m{half(price, True)}\033[m')
+    print(f'Com {tax}% of increase: \t\033[1;32m{increase(price, tax, True)}\033[m')
+    print(f'Com {taxing}% of decrease: \t\033[1;31m{decrease(price, taxing, True)}\033[m')
+    print('-' * 30)
+
---
+++ main.py
@@ -0,0 +1,5 @@
+from Utility import Money, Data
+
+
+price = float(input('Type the price: $'))
+Money.resume(price, 35, 22)
