354cadad01e0194352d0c1b1e9b6131cd610a629
thorcamara <thorcamaraalthoff@gmail.com>
2023-07-19 23:05:47 -0300

    Fixed text.

--- Utility/Money/__init__.py
+++ Utility/Money/__init__.py
@@ -26,7 +26,7 @@ def resume(price=0, tax=10, taxing=5):
     print(f'Price analyzed: \t\t\033[1;33m{money(price)}\033[m')
     print(f'Double of the price: \t\033[1;32m{double(price, True)}\033[m')
     print(f'Half of the price: \t\t\033[1;31m{half(price, True)}\033[m')
-    print(f'Com {tax}% of increase: \t\033[1;32m{increase(price, tax, True)}\033[m')
-    print(f'Com {taxing}% of decrease: \t\033[1;31m{decrease(price, taxing, True)}\033[m')
+    print(f'With {tax}% of increase: \t\033[1;32m{increase(price, tax, True)}\033[m')
+    print(f'With {taxing}% of decrease: \t\033[1;31m{decrease(price, taxing, True)}\033[m')
     print('-' * 30)
 
